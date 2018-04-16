d78e05a334ca8e63e6d2873c654baf745a61e773
Sweth Chandramouli <sweth.chandramouli@iinteractive.com>
2018-04-16 16:40:28 -0500

    5.24 adjust header component styling

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -3,7 +3,17 @@ import React from 'react';
 import { Text } from 'react-native';
 
 // make component
-const Header = () => <Text>Albums!</Text>;
+const Header = () => {
+  const { textStyle } = styles;
+
+  return <Text style={textStyle}>Albums!</Text>;
+};
+
+const styles = {
+  textStyle: {
+    fontSize: 20
+  }
+};
 
 // make component available to other parts of the app
 export default Header;
