2dd74ca887e8b32b82ee1e37e5a290edaa657b82
Sweth Chandramouli <sweth.chandramouli@iinteractive.com>
2018-04-16 14:31:21 -0500

    Got eslint stuff working (see commit message in full for details)
    Per https://rallycoding.com/blog/troubleshooting-react-native-startup/
    and https://github.com/evcohen/eslint-plugin-jsx-a11y/issues/348
    had to install specific versions of all eslint peerdeps and also had
    to update .eslintrc with  "rules": { "jsx-a11y/label-has-for": 'off' }
    
    Also had to add to ~/.vimrc
    
    let g:ale_linters = {'javascript': ['eslint']}
    
    so that it wouldn't use both eslint config and jshint config (which
    were conflicting, where eslint was insisting on semicolons and jshint
    was insisting on no extra semicolons

--- albums/index.ios.js
+++ albums/index.ios.js
@@ -1,12 +1,12 @@
 // import a library to help create a component
-import React from 'react'
-import ReactNative from 'react-native'
+import React from 'react';
+import ReactNative from 'react-native';
 
 // create a component
 const App = () => {
   return (
     <Text>Some Text</Text>
-  )
+  );
 };
 
 // render it to the device
