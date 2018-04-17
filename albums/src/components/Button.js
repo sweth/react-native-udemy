6f9a11fa38876309595efece0b8ab19f4a66be12
Sweth Chandramouli <sweth.chandramouli@iinteractive.com>
2018-04-17 16:48:37 -0500

    50 - style button

--- albums/src/components/Button.js
+++ albums/src/components/Button.js
@@ -2,9 +2,30 @@ import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
 const Button = () => (
-  <TouchableOpacity>
-    <Text>Click Me</Text>
+  <TouchableOpacity style={styles.button}>
+    <Text style={styles.buttonText}>Click Me</Text>
   </TouchableOpacity>
 );
+
+const styles = {
+  button: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff', // ios-blue
+    marginLeft: 5,
+    marginRight: 5,
+  },
+  buttonText: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+};
 
 export default Button;
