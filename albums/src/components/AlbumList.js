85c054732f0c98cdd1471a1d35b908b808b3b234
Sweth Chandramouli <sweth.chandramouli@iinteractive.com>
2018-04-17 11:55:37 -0500

    32 - add componentWillMount with debugger/console.log

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
+  componentWillMount() {
+    console.log('componentWillMount in AlbumList');
+    debugger;
+  }
+
   render() {
     return (
       <View>
