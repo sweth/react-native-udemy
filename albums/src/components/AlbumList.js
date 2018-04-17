f426b339ce1c322503ce8f6aa966ecca11a2b6cf
Sweth Chandramouli <sweth.chandramouli@iinteractive.com>
2018-04-17 13:59:35 -0500

    render minimalist version of each album from state

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -12,12 +12,20 @@ class AlbumList extends Component {
       ));
   }
 
+  renderAlbums() {
+    return (
+      this.state.albums.map(
+        album => <Text>{album.title}</Text>
+      )
+    );
+  }
+
   render() {
     console.log(this.state);
 
     return (
       <View>
-        <Text>Album List</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
