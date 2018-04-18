3b9173412cc9a202a63e7bfe09107d1f1d900b11
Sweth Chandramouli <sweth.chandramouli@iinteractive.com>
2018-04-18 15:32:16 -0500

    57 - copy common components from album app

---
+++ auth/src/components/common/Button.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+const Button = (props) => {
+  const {
+    pressHandler,
+    children: buttonContents,
+  } = props;
+  return (
+    <TouchableOpacity onPress={pressHandler} style={styles.button}>
+      <Text style={styles.buttonText}>{buttonContents}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = {
+  button: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff', // ios-blue
+    marginLeft: 5,
+    marginRight: 5,
+  },
+  buttonText: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600', // fontWeight must be a string not a num because reasons
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+};
+
+export default Button;
