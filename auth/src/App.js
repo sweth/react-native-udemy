ab779a346280a437bde882d97e8614d2b0f519b7
Sweth Chandramouli <sweth.chandramouli@iinteractive.com>
2018-04-18 16:16:01 -0500

    57 - add header component to app

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Header } from './components/common';
 
 class App extends Component {
-  render () {
+  render() {
     return (
-      <View><Text>App stub</Text></View>
-    )
+      <View>
+        <Header headerText="Authentication" />
+        <Text>App stub</Text>
+      </View>
+    );
   }
 }
 
