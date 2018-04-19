071d48f1350e9f11133328ef50dfffb80dee6958
Sweth Chandramouli <sweth.chandramouli@iinteractive.com>
2018-04-19 16:30:20 -0500

    60 - place stub loginform in app

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
   render() {
     return (
-      <View />
+      <Card>
+        <CardSection>
+        </CardSection>
+        <CardSection>
+        </CardSection>
+        <CardSection>
+          <Button>
+            Log In
+          </Button>
+        </CardSection>
+      </Card>
     );
   }
 }
