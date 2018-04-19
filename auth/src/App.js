c07018af7803f04893862c5371da4eac485f727b
Sweth Chandramouli <sweth.chandramouli@iinteractive.com>
2018-04-19 12:48:17 -0500

    59 - configure firebase on app load

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBTUK1ktV7Hj3jTVYXQEqcfN39JNt4BTjo',
+      authDomain: 'udemy-react-auth-test.firebaseapp.com',
+      databaseURL: 'https://udemy-react-auth-test.firebaseio.com',
+      projectId: 'udemy-react-auth-test',
+      storageBucket: 'udemy-react-auth-test.appspot.com',
+      messagingSenderId: '456135189449',
+    });
+  }
+
   render() {
     return (
       <View>
